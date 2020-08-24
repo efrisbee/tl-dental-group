49eb13a4c7f6adef92840f5b3dce2d0ae718cabc
nathanaskinner <naskinner@naskinner.local>
2020-08-23 21:34:41 -0400

    initial eleventy setup

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dir: {
+        input: "views",
+        output: "dist",
+        includes: "includes",
+        layouts: "layouts",
+        data: "data"
+    },
+    templateFormats: ["html", "liquid", "md"],
+    htmlTemplateEngine: "liquid"
+  };
