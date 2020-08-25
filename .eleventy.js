64a9e7a4bcd126e5cac17de95c221a0f50045182
nathanaskinner <naskinner@naskinner.local>
2020-08-25 09:09:22 -0400

    sass set up

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,17 @@
-module.exports = {
-    dir: {
-        input: "views",
-        output: "dist",
-        includes: "includes",
-        layouts: "layouts",
-        data: "data"
-    },
-    templateFormats: ["html", "liquid", "md"],
-    htmlTemplateEngine: "liquid"
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy('views/assets/styles/css');
+
+    return {
+        dir: {
+            input: "views",
+            output: "dist",
+            includes: "includes",
+            layouts: "layouts",
+            data: "data"
+        },
+        templateFormats: ["html", "liquid", "md"],
+        htmlTemplateEngine: "liquid",
+        passthroughFileCopy: true
+      };
   };
+
